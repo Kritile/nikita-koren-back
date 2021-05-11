de4a67fa5691d04532fcd550ecf540e5dff20ab3
Kritile <rusakov@ittest-team.ru>
2021-05-11 19:12:54 +0300

    change post to get

--- api/users/login/index.php
+++ api/users/login/index.php
@@ -3,5 +3,5 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 include "../../../model/user.php";
 $user = new User();
-echo json_encode( $user->login($_POST['login'],$_POST['pass']));
+echo json_encode( $user->login($_GET['login'],$_GET['pass']));
 
