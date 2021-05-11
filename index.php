1d907fca1a8de9a2d5b2abda429adbb32ed3b2a2
Kritile <rusakov@ittest-team.ru>
2021-05-11 15:28:06 +0300

    create login

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-//$user = new User(1);
+$user = new User();
+$user_check = $user->login('user','pass');
+var_dump($user_check);
 ////var_dump(1);
-$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"],1);
-$link = mysqli_connect($server, $username, $password);
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}else{
-    $user = mysqli_query($link,'SELECT * FROM user WHERE `id` = 1');
-    var_dump($user);
-}
+
--- model/user.php
+++ model/user.php
@@ -5,11 +5,26 @@ class User{
     private $id;
     private $login;
     private $pass;
+    private $link;
 
-    public function __construct($id)
+    public function __construct()
     {
+        $url= parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"],1);
+        $this->link = mysqli_connect($server, $username, $password);
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        }
 
 
+    }
+    public function login($user_login, $password) {
+        $user = mysqli_query($this->link, 'SELECT * FROM users WHERE `login` = '.$user_login);
+        return $user;
     }
 
 
