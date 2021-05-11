8f6cad39f4fd511d23f7feb6f7293a63d43d83fa
Kritile <rusakov@ittest-team.ru>
2021-05-11 19:09:14 +0300

    dispay  errors

--- api/users/login/index.php
+++ api/users/login/index.php
@@ -1,12 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 include "../../../model/user.php";
-var_dump($_POST);
-echo "<pre>".print_r($GLOBALS, true)."</pre>";
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if($_SERVER['REQUEST_METHOD'] == "GET"){
     $user = new User();
-    echo json_encode( $user->login($_POST['login'],$_POST['pass']));
+    echo json_encode( $user->login($_GET['login'],$_GET['pass']));
 }else{
     echo 'Only POST request';
 }
