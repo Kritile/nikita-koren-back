41879f4b3a239911b56ebff80530dce788b43306
Kritile <rusakov@ittest-team.ru>
2021-05-11 18:58:20 +0300

    Add login/register API

--- api/users/login/index.php
+++ api/users/login/index.php
@@ -1,7 +1,7 @@
 <?php
 include "../../../model/user.php";
 var_dump($_POST);
-
+echo "<pre>".print_r($GLOBALS, true)."</pre>";
 if(isset($_POST['login']) && isset($_POST['pass'])){
     $user = new User();
     echo json_encode( $user->login($_POST['login'],$_POST['pass']));
