07a36772ddc6f8b41c9b31f79e7877bda1c487f3
Kritile <rusakov@ittest-team.ru>
2021-05-11 15:20:54 +0300

    qwe

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
-$user = new User(1);
-var_dump(1);
+//$user = new User(1);
+////var_dump(1);
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
+$link = mysqli_connect($server, $username, $password);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}else{
+    $user = mysqli_query($link,'SELECT * FROM user WHERE `id` ='.id);
+    var_dump($user);
+}
--- model/user.php
+++ model/user.php
@@ -8,18 +8,8 @@ class User{
 
     public function __construct($id)
     {
-        $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $server = $url["host"];
-        $username = $url["user"];
-        $password = $url["pass"];
-        $db = substr($url["path"],1);
-        $mysqli = new mysqli($server, $username, $password);
-        if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-        $user = $mysqli->query('SELECT * FROM user WHERE `id` ='.id);
-        var_dump($user);
+
+
     }
 
 
