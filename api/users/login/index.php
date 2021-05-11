50b470b8b285bd749dbe6a8377cba42c5f67332c
Kritile <rusakov@ittest-team.ru>
2021-05-11 19:02:26 +0300

    dispay  errors

--- api/users/login/index.php
+++ api/users/login/index.php
@@ -1,8 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 include "../../../model/user.php";
 var_dump($_POST);
 echo "<pre>".print_r($GLOBALS, true)."</pre>";
-if(isset($_POST['login']) && isset($_POST['pass'])){
+if($_SERVER['REQUEST_METHOD'] == "POST"){
     $user = new User();
     echo json_encode( $user->login($_POST['login'],$_POST['pass']));
 }else{
