1ffed7b26eb9522bd3f933214a08c7d7216f144f
Kritile <rusakov@ittest-team.ru>
2021-06-20 17:21:06 +0300

    add group.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include './model/user.php';
 include './model/table.php';
 include './model/card.php';
+include './model/group.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-include "../../../model/group.php";
 $card = new Group();
 echo '<pre>';
 var_dump($card->get_name(5));
